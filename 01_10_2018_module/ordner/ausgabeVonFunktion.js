868c8fdf887ac7d7f922383b8559c0bc7e997c58
DavidHieselmayr <david.hieselmayr@gmail.com>
2018-10-13 16:44:33 +0200

    11_10_2018_Objekte_genauereAusgabe(mehr Infos)

---
+++ 01_10_2018_module/ordner/ausgabeVonFunktion.js
@@ -0,0 +1,6 @@
+var name = 'Susi';
+console.log('name =', name, name.constructor);
+var ok = true;
+console.log('ok = ', ok, ok.constructor);
+var total = 100;
+console.log('total = ', total, total.constructor);
