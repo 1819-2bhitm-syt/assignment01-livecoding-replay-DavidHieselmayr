316e706f17b2be820c9227fc0b8b6cbb3a5eca06
DavidHieselmayr <david.hieselmayr@gmail.com>
2018-11-18 19:32:54 +0100

    15_11_2018 Objektorientierung III

--- 01_10_2018_module/ordner/app.js
+++ 01_10_2018_module/ordner/app.js
@@ -2,7 +2,9 @@ function Person(vorname,nachname) {
     this.vorname = vorname;
     this.nachname = nachname;
 }
-
+Person.prototype.getName = function (){
+    return this.vorname + " " + this.nachname
+};
 
 let hans = new Person("Hans", "Müller");
-console.log(hans);
+console.log(hans.getName());
