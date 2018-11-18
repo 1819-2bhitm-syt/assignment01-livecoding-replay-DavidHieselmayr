a1cc985195fbca982794789b3dc29a72dcd3f6df
DavidHieselmayr <david.hieselmayr@gmail.com>
2018-11-18 19:27:29 +0100

    15_11_2018 Debuggen

--- 01_10_2018_module/ordner/app.js
+++ 01_10_2018_module/ordner/app.js
@@ -1,3 +1,11 @@
-const os = require("os");
+function Person(vorname,nachname) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+}
 
-console.log(os.cpus());
+Person.prototype.getName = function () {
+    return this.vorname + " " + this.nachname
+};
+
+let hans = new Person("Hans", "Müller");
+console.log(hans.getName());
